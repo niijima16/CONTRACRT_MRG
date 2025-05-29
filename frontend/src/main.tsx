4d504ebaa301db91a6740f408d5b750baca0e26b
niijima16 <niijima.16@gmail.com>
2025-05-29 23:43:54 +0900

    update with the frontend 0529T2343

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,10 @@
-// /frontend/src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import EditContract from './pages/EditContract';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/contracts/:id/edit" element={<EditContract />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
