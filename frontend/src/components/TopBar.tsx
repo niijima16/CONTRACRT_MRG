b369ffbd64ae45fadf5e39d6ba19414d59e25e8f
niijima16 <niijima.16@gmail.com>
2025-07-03 09:59:17 +0900

    update branch 0703T0959

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -9,9 +9,10 @@ const TopBar: React.FC = () => (
   <header className={styles.topbar}>
     <nav className={styles.nav}>
       <Link to="/contracts">契約情報</Link>
+      <Link to="/invoices">請求書</Link>
       <Link to="/clients">契約先情報</Link>
       <Link to="/sales">売上情報</Link>
-      {/* 必要に応じて追加 */}
+
     </nav>
   </header>
 )
